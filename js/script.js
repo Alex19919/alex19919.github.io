bb849657b69526374adff27cb0ad283882e93c1b
Oleksandr <96943240+Alex19919@users.noreply.github.com>
2024-02-10 19:57:42 +0200

    myProject CraftyCorner

--- js/script.js
+++ js/script.js
@@ -2,3 +2,9 @@ document.addEventListener("DOMContentLoaded", function() {
     document.body.style.opacity = "1";
     document.querySelector(".section").style.opacity = "1";
 });
+
+function toggleTheme() {
+            const body = document.body;
+            body.classList.toggle('light-mode');
+            body.classList.toggle('dark-mode');
+        }
