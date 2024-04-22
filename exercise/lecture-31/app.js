ee40e4e26b4c7c5eb9e4146b6959a178175d0b6e
Alex19919 <alex.ulyanovych@gmail.com>
2024-04-22 21:14:06 +0300

    fix lecture 30-32  .... strart lecture 33

--- exercise/lecture-31/app.js
+++ exercise/lecture-31/app.js
@@ -1,17 +1,17 @@
 const url = "https://jsonplaceholder.typicode.com/posts";
 
 const template = (item) => `
-<h3>${item.title}</h3>
-<div>${item.body}</div>
-<p>Author: <strong><span class="author" data-id="${item.userId}"></span></strong></p>
+  <h3>${item.title}</h3>
+  <div>${item.body}</div>
+  <p>Author: <strong><span class="author" data-id="${item.userId}"></span></strong></p>
 `;
 
 const xhrPromise = (method, url) => {
-  const promise = new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     const xhr = new XMLHttpRequest();
     xhr.open(method, url);
     xhr.send();
-  
+
     xhr.onload = () => {
       if (xhr.status >= 400) {
         reject(xhr.response);
@@ -21,29 +21,43 @@ const xhrPromise = (method, url) => {
     };
 
     xhr.onerror = () => {
-      reject('Something went wrong!');
-    };   
+      reject("Something went wrong!");
+    };
   });
+};
 
-  return promise;
+const usersCache = {};
+
+const getUserInfo = async (userId) => {
+  if (usersCache[userId]) {
+    return usersCache[userId];
+  } else {
+    const response = await xhrPromise(
+      "GET",
+      `https://jsonplaceholder.typicode.com/users/${userId}`
+    );
+    const userInfo = JSON.parse(response);
+    usersCache[userId] = userInfo;
+    return userInfo;
+  }
 };
 
 xhrPromise("GET", url)
-.then(response => {
-    const posts = JSON.parse(response)
-		let result = ''
-    posts.forEach(item => {
-        result += template(item)
-    })
-    document.getElementById("blog").innerHTML = result;    
-})
-.then(() => {
-    const authors = document.querySelectorAll('.author');
-    authors.forEach(author => {
-        xhrPromise('GET', `https://jsonplaceholder.typicode.com/users/${author.dataset.id}`)
-        .then(response => {
-            let authorName = JSON.parse(response);
-            author.textContent = authorName.name;
-        })
-    })
-})
+  .then(async (response) => {
+    const posts = JSON.parse(response);
+    let result = "";
+    for (const item of posts) {
+      result += template(item);
+    }
+    document.getElementById("blog").innerHTML = result;
+
+    const authorElements = document.querySelectorAll(".author");
+    for (const authorElement of authorElements) {
+      const userId = authorElement.dataset.id;
+      const userInfo = await getUserInfo(userId);
+      authorElement.textContent = userInfo.name;
+    }
+  })
+  .catch((error) => {
+    console.error(error);
+  });
--- exercise/lecture-32/app.js
+++ exercise/lecture-32/app.js
@@ -1,35 +1,64 @@
 const url = "https://jsonplaceholder.typicode.com/posts";
 
 const template = (item) => `
-<h3>${item.title}</h3>
-<div>${item.body}</div>
-<p>Author: <strong><span class="author" data-id="${item.userId}"></stan></strong></p>
+  <h3>${item.title}...</h3>
+  <div>${item.body}...</div>
+  <p>Author: <strong><span class="author" data-id="${item.userId}"></span></strong></p>
 `;
 
-async function fetchData(url) {
-    return await fetch(url, {
-        method: 'GET',
-        headers: { "Content-Type": "application/json" }
-    })
-        .then((response) => {
-            const posts = JSON.parse(response);
-            let result = "";
-            posts.forEach((item) => {
-                result += template(item);
-            });
-            document.getElementById("blog").innerHTML = result;
-        })
+const fetchPosts = async () => {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error("Failed to fetch posts");
+    }
+    const posts = await response.json();
+    return posts;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
 
-        .then(() => {
-            const users = document.querySelectorAll(".author");
-            users.forEach((user) => {
-                  fetch( 
-                    `https://jsonplaceholder.typicode.com/posts/${user.dataset.id}`
-                ).then((response) => {
-                    let userName = JSON.parse(response);
-                    console.log(userName.name);
-                    user.textContent = userName.name;
-                });
-            });
-        });
-}
+const fetchUserInfo = async (userId) => {
+  try {
+    if (usersCache[userId]) {
+      return usersCache[userId];
+    } else {
+      const response = await fetch(
+        `https://jsonplaceholder.typicode.com/users/${userId}`
+      );
+      if (!response.ok) {
+        throw new Error("Failed to fetch user info");
+      }
+      const userInfo = await response.json();
+      usersCache[userId] = userInfo;
+      return userInfo;
+    }
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+const usersCache = {};
+
+(async () => {
+  try {
+    const posts = await fetchPosts();
+    let result = "";
+    for (const item of posts) {
+      result += template(item);
+    }
+    document.getElementById("blog").innerHTML = result;
+
+    const authorElements = document.querySelectorAll(".author");
+    for (const authorElement of authorElements) {
+      const userId = authorElement.dataset.id;
+      const userInfo = await fetchUserInfo(userId);
+      authorElement.textContent = userInfo.name;
+    }
+  } catch (error) {
+    console.error(error);
+  }
+})();
